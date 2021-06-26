7aea5e91a3d7188a0dbe410a6be8ed77bdbe3f12
SakibSaikia <sakib.r.saikia@gmail.com>
2021-06-25 17:25:54 -0700

    Environment BRDF

--- demo-dll/shaders/env-brdf-integration.hlsl
+++ demo-dll/shaders/env-brdf-integration.hlsl
@@ -29,7 +29,35 @@ void cs_main(uint3 dispatchThreadId : SV_DispatchThreadID)
     if (dispatchThreadId.x < g_computeConstants.outputUavWidth &&
         dispatchThreadId.y < g_computeConstants.outputUavHeight)
     {
+        float2 res = 0.f.xx;
+        const float NoV = dispatchThreadId.x / (float)g_computeConstants.outputUavWidth;
+        const float Roughness = 1.f - dispatchThreadId.y / (float)g_computeConstants.outputUavHeight;
+        const uint NumSamples = g_computeConstants.sampleCount;
+
+        float3 V = float3(sqrt(1.f - NoV * NoV), 0.f, NoV);
+        float3 N = float3(0.f, 0.f, 1.f);
+
+        float A = 0.f;
+        float B = 0.f;
+
+        for (uint i = 0; i < NumSamples; i++)
+        {
+            float2 Xi = Hammersley(i, NumSamples);
+            float3 H = ImportanceSampleGGX(Xi, Roughness, N);
+            float3 L = 2.f * dot(V, H) * H - V;
+
+            float NoL = saturate(L.z);
+            float NoH = saturate(H.z);
+            float VoH = saturate(dot(V, H));
+
+            float G = G_Smith_IBL(NoV, NoL, Roughness);
+            float G_Vis = G * VoH / (NoH * NoV);
+            float Fc = pow(1 - VoH, 5);
+            A += (1 - Fc) * G_Vis;
+            B += Fc * G_Vis;
+        }
+
         RWTexture2D<float2> dest = g_uavBindless2DTextures[g_computeConstants.outputUavIndex];
-        dest[uint2(dispatchThreadId.x, dispatchThreadId.y)] = float2(0.2, 0.7);
+        dest[uint2(dispatchThreadId.x, dispatchThreadId.y)] = float2(A, B) / NumSamples;
     }
 }
