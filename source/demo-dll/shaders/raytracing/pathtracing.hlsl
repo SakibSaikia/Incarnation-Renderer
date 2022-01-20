610d88f456fcd56aec88184483390cb9f63441cd
SakibSaikia <sakib.r.saikia@gmail.com>
2022-01-19 23:58:59 -0800

    Added emissive contribution to path tracing

--- source/demo-dll/shaders/raytracing/pathtracing.hlsl
+++ source/demo-dll/shaders/raytracing/pathtracing.hlsl
@@ -155,6 +155,11 @@ void chsMain(inout RayPayload payload, in BuiltInTriangleIntersectionAttributes 
     FMaterial material = MeshMaterial::GetMaterial(primitive.m_materialIndex, globalMaterialBufferIndex);
     FMaterialProperties matInfo = EvaluateMaterialProperties(material, uv, g_trilinearSampler);
 
+    if (any(matInfo.emissive))
+    {
+        payload.color.xyz += matInfo.emissive * 20000;
+    }
+
 #if LIGHTING_ONLY
     matInfo.basecolor = 0.5.xxx;
 #endif
