a0696f2fc0bcc86463a0cc5ca445c9a2e1b3e781
SakibSaikia <sakib.r.saikia@gmail.com>
2023-01-05 15:45:45 -0800

    Fixed higlight pass rootsig

--- source/demo-dll/shaders/geo-raster/highlight-pass.hlsl
+++ source/demo-dll/shaders/geo-raster/highlight-pass.hlsl
@@ -6,7 +6,8 @@
 	"RootFlags( CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED )," \
     "RootConstants(b0, num32BitConstants=32, visibility = SHADER_VISIBILITY_ALL)," \
     "CBV(b1, visibility = SHADER_VISIBILITY_ALL)," \
-    "CBV(b2, visibility = SHADER_VISIBILITY_ALL)"
+    "CBV(b2, visibility = SHADER_VISIBILITY_ALL), " \
+	"StaticSampler(s0, filter = FILTER_ANISOTROPIC, addressU = TEXTURE_ADDRESS_WRAP, addressV = TEXTURE_ADDRESS_WRAP, borderColor = STATIC_BORDER_COLOR_OPAQUE_WHITE)"
 
 struct FPassConstants
 {
